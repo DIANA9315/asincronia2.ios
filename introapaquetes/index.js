13a44bbe1e705c17ed5b6ca705cd082d15bce86f
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-07-30 12:49:20 -0600

    modulos

--- introapaquetes/index.js
+++ introapaquetes/index.js
@@ -9,5 +9,6 @@ console.log("!Hola Mundo")
 let texto=numberToText.convertToText(12346);
 console.log(texto); // "twelve thousand three 
 // hundred forty-six"
+
 texto=numberToText.convertToText("54321");
 console.log(texto); // "fifty-four thousand three hundred twenty-one"
