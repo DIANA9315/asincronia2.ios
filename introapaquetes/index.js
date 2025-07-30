fa5fce855bbffd2dffde9850e496fff468891e0e
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-07-30 12:48:28 -0600

    Initial commit of asincronia2 project

---
+++ introapaquetes/index.js
@@ -0,0 +1,13 @@
+const numberToText = require('number-to-text')
+require('number-to-text/converters/en-us');
+console.log("!Hola Mundo")
+/**crear una aplicscion que conveirta un vslor numerico a texto */
+
+
+
+
+let texto=numberToText.convertToText(12346);
+console.log(texto); // "twelve thousand three 
+// hundred forty-six"
+texto=numberToText.convertToText("54321");
+console.log(texto); // "fifty-four thousand three hundred twenty-one"
