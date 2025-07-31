ab9e2c6392414d89a985ae006419f25ce12ad5ca
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-07-31 12:35:45 -0600

    planetario

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,39 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+   {
+    nombre: "Urano",
+    descripcion: "Un gigante de hielo similar a neptuno  .",
+    descubiertoEn: "1781"
+  },
+    { nombre: "Neptuno",
+    descripcion: "Un helado gingante .",
+    descubiertoEn: "1846"
+  },
+     { nombre: "Pluton",
+     descripcion: "Un planeta enano con hogar de su luna mas grande.",
+    descubiertoEn: "1930"
+  },
+      { nombre: "Saturno,encelado es una luna de saturno con un oceano de agua liquida bajo su superficie helada.",
+     descripcion: "Un planeta de anillos.",
+    descubiertoEn: "1789"
+  },
+      { nombre: "Caronte, Pluton.",
+     descripcion: "Sistema binario.",
+    descubiertoEn: "1978"
+  },
+      { nombre: "Jupiter, las cuatro lunas galileanas son Ío, Europa, Ganimedes y Calisto.",
+     descripcion: "Las lunas mas grandes , Europa luna helada, Ganimedes es la luna mas grande del sistema solar con superficie helada y oceano, Calisto es la tercera luna mas  grande de jupiter y la segunda despues de Ganimedes.",
+    descubiertoEn: "1910"
+  },
+ 
+];
+module.exports = planetas;
